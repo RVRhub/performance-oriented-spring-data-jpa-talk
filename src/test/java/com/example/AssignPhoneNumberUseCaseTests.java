1bc6c3fc1f5e62a426f9414a9e513652ba04d640
Roman Rybak <r.romanv1@gmail.com>
2024-06-03 16:53:23 +0200

    - improve transactions - add lazy load and use EntityGraph - remove redundent calls for AssignPhoneNumber

--- src/test/java/com/example/AssignPhoneNumberUseCaseTests.java
+++ src/test/java/com/example/AssignPhoneNumberUseCaseTests.java
@@ -3,6 +3,10 @@ package com.example;
 import com.example.domain.PhoneNumber;
 import org.junit.jupiter.api.Test;
 
+import org.quickperf.sql.annotation.ExpectDelete;
+import org.quickperf.sql.annotation.ExpectInsert;
+import org.quickperf.sql.annotation.ExpectSelect;
+import org.quickperf.sql.annotation.ExpectUpdate;
 import org.springframework.beans.factory.annotation.Autowired;
 
 @UseCaseTest
@@ -12,6 +16,9 @@ class AssignPhoneNumberUseCaseTests {
     private AssignPhoneNumberUseCase useCase;
 
     @Test
+    @ExpectSelect(1)
+    @ExpectInsert(1)
+    @ExpectDelete(0)
     void execute() {
         useCase.execute("sender-id", new PhoneNumber("00334455", PhoneNumber.Type.HOME));
     }
